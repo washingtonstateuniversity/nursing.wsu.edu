04fb300dc1523b4411df73b0c5f59978b468ab96
jeremyfelt <jeremy.felt@gmail.com>
2015-07-08 15:12:54 -0700

    Setup initial navigation areas

---
+++ parts/footers.php
@@ -0,0 +1,19 @@
+<?php
+
+$wsu_fat_footer_args = array(
+	'theme_location'  => 'fat-footer',
+	'menu'            => 'fat-footer',
+	'container'       => 'div',
+	'container_class' => false,
+	'container_id'    => 'fat-footer',
+	'menu_class'      => null,
+	'menu_id'         => null,
+	'items_wrap'      => '<ul>%3$s</ul>',
+	'depth'           => 3,
+);
+?>
+<footer class="single row fat-footer-menu">
+	<div class="column one">
+		<?php wp_nav_menu( $wsu_fat_footer_args ); ?>
+	</div>
+</footer>
