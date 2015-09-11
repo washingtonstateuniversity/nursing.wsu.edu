a7869c311f7e778b109be9aeb23a7c571dd637b4
jeremyfelt <jeremy.felt@gmail.com>
2015-09-11 09:05:28 -0700

    Remove registration of extra menus

--- functions.php
+++ functions.php
@@ -5,29 +5,8 @@ class WSU_Nursing {
 	 * Setup the hooks used by the theme.
 	 */
 	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'register_menus' ), 10 );
 		add_action( 'widgets_init', array( $this, 'setup_sidebars' ), 10 );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 20 );
-	}
-
-	/**
-	 * Register the menus used by the College of Nursing.
-	 */
-	public function register_menus() {
-		// The top menu on the home page, above the mega menu.
-		register_nav_menu( 'top-menu', 'Top Menu' );
-
-		// The primary portion of the Mega Menu at the top of the home page.
-		register_nav_menu( 'mega-menu', 'Mega Menu' );
-
-		// A section of the Mega Menu, visible when expanded, highlighting signature links.
-		register_nav_menu( 'signature-menu', 'Signature Menu' );
-
-		// A section for campus locations, visible when expanded.
-		register_nav_menu( 'top-level-links', 'Campus Locations' );
-
-		// Items that appear next to the search bar as quick links.
-		register_nav_menu( 'quick-links', 'Search Links' );
 	}
 
 	/**
