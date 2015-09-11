72f4adf16400a15a443193dd2d87c3891bdba237
jeremyfelt <jeremy.felt@gmail.com>
2015-09-11 09:06:30 -0700

    Remove Navigation JS and related code

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ class WSU_Nursing {
 	 */
 	public function __construct() {
 		add_action( 'widgets_init', array( $this, 'setup_sidebars' ), 10 );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 20 );
 	}
 
 	/**
@@ -20,11 +19,6 @@ class WSU_Nursing {
 			'after_widget'  => '</div>',
 		);
 		register_sidebar( $fat_footer_args );
-	}
-
-	public function enqueue_scripts() {
-		wp_enqueue_script( 'wsu-nursing-navigation', get_stylesheet_directory_uri() . '/js/navigation.js', array( 'jquery', 'backbone' ), spine_get_script_version(), true );
-		wp_enqueue_script( 'wsu-nursing-primary', get_stylesheet_directory_uri() . '/js/primary.js', array( 'wsu-nursing-navigation' ), spine_get_script_version(), true );
 	}
 }
 new WSU_Nursing();
