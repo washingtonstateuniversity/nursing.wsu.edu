83462fe8830c4c6bdf738c975edcf7911ecbd78e
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-30 09:52:28 -0700

    Take away site depth
    Take away site depth

--- parts/before-main.php
+++ parts/before-main.php
@@ -1,14 +1,15 @@
-<div class="">
-	<?php
-$hrs_common_search_args = array(
-	'theme_location'  => 'nursing-top-menu',
-	'menu'            => 'nursing-top-menu',
-	'container'       => 'div',
-	'container_class' => false,
-	'container_id'    => 'nursing-top-menu',
-	'menu_class'      => null,
-	'menu_id'         => null,
-	'items_wrap'      => '<ul class="header-top">%3$s</ul>',
-	'depth'           => 2,
-);
+<div class="site-top">
+		<?php
+	$spine_site_args = array(
+		'theme_location'  => 'site',
+		'menu'            => 'topmenu',
+		'container'       => false,
+		'container_class' => false,
+		'container_id'    => false,
+		'menu_class'      => null,
+		'menu_id'         => null,
+		'items_wrap'      => '<ul class="header-menu">%3$s</ul>',
+		'depth'           => 5,
+	);
 	wp_nav_menu( $spine_site_args ); ?>
+	</div>
